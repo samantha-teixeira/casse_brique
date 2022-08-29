42c6fb908a2df38f4c057b72f184e71d2bff1e0d
samantha-teixeira <samerika69@gmail.com>
2022-08-29 13:53:47 +0200

    a

---
+++ main.js
@@ -0,0 +1,41 @@
+let canvas = document.getElementById("myCanvas");
+let ctx = canvas.getContext("2d");
+
+var x = canvas.width/2;
+var y = canvas.height-30;
+var dx = 2;
+var dy = -2;
+
+let ballRadius = 10;
+
+function drawBall() {
+    ctx.beginPath();
+    ctx.arc(x, y, ballRadius, 0, Math.PI*2);
+    ctx.fillStyle = "#0095DD";
+    ctx.fill();
+    ctx.closePath();
+  }
+  
+  function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBall();
+    x += dx;
+    y += dy;
+
+    if (y + dy <ballRadius || y + dy > canvas.height-ballRadius) {
+        dy = -dy
+        ctx.fillStyle = "#02d909";
+        ctx.fill();
+    }
+    
+    if (x + dx <ballRadius || x + dx > canvas.width-ballRadius) {
+        dx = -dx
+        ctx.fillStyle = "#e84b02";
+        ctx.fill();
+    }
+    
+  }
+
+setInterval(draw, 10);
+
+
